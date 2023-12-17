6435506cbd13ae2ab64bacd9951359b9e19e90a8
Beeranna Dude <101712802+beeru405@users.noreply.github.com>
2023-12-17 19:17:42 +0530

    Update jenkins.sh

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade -y
-sudo apt install openjdk-11-jdk
+sudo apt install openjdk-11-jdk -y
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
 sudo sh -c 'echo deb [trusted=yes] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
